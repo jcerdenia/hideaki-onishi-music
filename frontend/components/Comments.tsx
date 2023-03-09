ebe4df7243e89b25d8e0444fcbebf7020d677081
Joshua Cerdenia <joshcerdenia@gmail.com>
2023-03-09 17:10:08 +0800

    feat(Comments): add bg

--- frontend/components/Comments.tsx
+++ frontend/components/Comments.tsx
@@ -2,15 +2,17 @@ import { DiscussionEmbed } from "disqus-react";
 
 const Comments = ({ post }: any) => {
   return (
-    <DiscussionEmbed
-      shortname="hideakionishimusic"
-      config={{
-        url: post.url,
-        identifier: post.slug,
-        title: post.title,
-        language: "en",
-      }}
-    />
+    <div className="bg-slate-100 p-4 rounded-md shadow">
+      <DiscussionEmbed
+        shortname="hideakionishimusic"
+        config={{
+          url: post.url,
+          identifier: post.slug,
+          title: post.title,
+          language: "en",
+        }}
+      />
+    </div>
   );
 };
 
